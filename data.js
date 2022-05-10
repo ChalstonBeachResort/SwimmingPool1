e609c2ac099dd360399cd5d84a989dae06655e30
Sanju Kamat <sanjukamat123@gmail.com>
2022-05-10 11:39:33 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5697541818003984,
+        "pitch": 0.1775306013428306,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35871838819779533,
+          "pitch": 0.011597654654750045,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": 1.3834000826616055,
+          "pitch": 0.01681799854391386,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -1.202691518275099,
+          "pitch": 0.004854440229859591,
+          "rotation": 0,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.583577684920675,
+        "pitch": 0.295516790937679,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.894097946590133,
+          "pitch": 0.05370836024614434,
+          "rotation": 0,
+          "target": "0-3"
+        },
+        {
+          "yaw": 1.980813749046023,
+          "pitch": 0.015008108646128449,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -1.787197661242942,
+          "pitch": 0.02415245291209267,
+          "rotation": 0,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.603867606481387,
+        "pitch": 0.10161649285039864,
+        "fov": 1.1405458339398038
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26191788862671217,
+          "pitch": 0.03018157394138754,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.8719631514070176,
+        "pitch": 0.2970371767948521,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.303165319653198,
+          "pitch": 0.016816984998072826,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Swimming Pool",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
